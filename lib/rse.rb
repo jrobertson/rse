5011b58e861fea1eae1735cf9e46c2d867a8dcd1
James Robertson <james@jessie.qbitx.com>
2019-03-25 22:07:33 +0000

    improvement: Auxillary services including registry, sps, and spslog are now optional.

--- lib/rse.rb
+++ lib/rse.rb
@@ -13,46 +13,50 @@ require 'spspublog_drb_client'
 class Rse
   using ColouredText
   
-  def initialize(package_basepath=nil, host: '', port: '61000', 
-                 debug: false, loghost: 'localhost', logport: '9090', 
-                 reghost: 'localhost', spshost: 'localhost', app_rsf: nil)
+  def initialize(package_basepath=nil, host: '0.0.0.0', port: '61000', 
+                 debug: false, loghost: nil, logport: '9090', 
+                 reghost: nil, spshost: nil, app_rsf: nil)
 
     @host, @port, @debug = host, port, debug
 
     puts 'before spspublog'.info if @debug
-    log = SPSPubLogDRbClient.new host: loghost, port: logport
+    log = loghost ? SPSPubLogDRbClient.new(host: loghost, port: logport) : nil
     
     puts 'before reg'.info if @debug
-    reg = RemoteDwsRegistry.new domain: reghost   
+    reg = reghost ? RemoteDwsRegistry.new(domain: reghost) : nil
 
     @rs = rs = RSFServices.new reg, package_basepath: package_basepath, 
         log: log, app_rsf: app_rsf, debug: debug
     
-    sps = SPSSub.new host: spshost
-       
-    Thread.new do
-      sps.subscribe(topic: 'rse/#') do |msg,topic|
-        a = topic.split('/')[1..-1]
-        if a.length < 2 then
-          begin
-            r = rs.run_job(a.first, msg)
-          rescue
-            sps.notice 'rse_result: no job ' + a.first
+    sps = spshost ? SPSSub.new(host: spshost) : nil
+
+    if sps then
+        
+      Thread.new do
+        sps.subscribe(topic: 'rse/#') do |msg,topic|
+          a = topic.split('/')[1..-1]
+          if a.length < 2 then
+            begin
+              r = rs.run_job(a.first, msg)
+            rescue
+              sps.notice 'rse_result: no job ' + a.first
+            end
+            sps.notice 'rse_result: ' + r.inspect
+          else
+            package, job = a
+            begin
+              r = rs.run_job(package, job, {}, msg)
+            rescue
+              sps.notice 'rse_result: no job ' + a.first
+            end
+            sps.notice "rse_result/%s/%s: %s" % [package, job, r.inspect ]
           end
-          sps.notice 'rse_result: ' + r.inspect
-        else
-          package, job = a
-          begin
-            r = rs.run_job(package, job, {}, msg)
-           rescue
-            sps.notice 'rse_result: no job ' + a.first
-           end
-          sps.notice "rse_result/%s/%s: %s" % [package, job, r.inspect ]
         end
       end
-    end
 
-    @rs.services['sps'] = sps
+      @rs.services['sps'] = sps
+      
+    end
 
     puts 'ready'
 
--- rse.gemspec
+++ rse.gemspec
@@ -1,13 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'rse'
-  s.version = '0.1.2'
-  s.summary = 'Executes Ruby jobs (using the rsf_services gem) from a DRb server.'
+  s.version = '0.2.0'
+  s.summary = 'Executes Ruby jobs (using the rsf_services gem) from ' + 
+      'a DRb server.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/rse.rb']
-  s.add_runtime_dependency('sps-sub', '~> 0.3', '>=0.3.6')
-  s.add_runtime_dependency('rsf_services', '~> 0.7', '>=0.7.1')
-  s.add_runtime_dependency('remote_dwsregistry', '~> 0.2', '>=0.2.7')
-  s.add_runtime_dependency('spspublog_drb_client', '~> 0.1', '>=0.1.0')
+  s.add_runtime_dependency('sps-sub', '~> 0.3', '>=0.3.7')
+  s.add_runtime_dependency('rsf_services', '~> 0.8', '>=0.8.0')
+  s.add_runtime_dependency('remote_dwsregistry', '~> 0.4', '>=0.4.1')
+  s.add_runtime_dependency('spspublog_drb_client', '~> 0.1', '>=0.1.1')
   s.signing_key = '../privatekeys/rse.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
