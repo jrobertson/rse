44a1826ae289c4e4f6fe9d0c654678a9ac1bcbe7
James Robertson <james@jamesrobertson.me.uk>
2021-12-13 21:51:07 +0000

    feature: A job filter can now be applied which will send a job to a specific server when using the proxy.

--- lib/rse.rb
+++ lib/rse.rb
@@ -12,27 +12,32 @@ require 'spspublog_drb_client'
 
 
 module Rse
-  
+
   class Server
     using ColouredText
-    
-    def initialize(package_basepath=nil, host: '0.0.0.0', port: '61000', 
-                  debug: false, loghost: nil, logport: '9090', 
+
+    def initialize(package_basepath=nil, host: '0.0.0.0', port: '61000',
+                  debug: false, loghost: nil, logport: '9090', log: nil,
                   reghost: nil, spshost: nil, app_rsf: nil)
 
       @host, @port, @debug = host, port, debug
 
       puts 'before spspublog'.info if @debug
-      log = loghost ? SPSPubLogDRbClient.new(host: loghost, port: logport) : nil
-      
+
+      log2 = if log then
+        log
+      elsif loghost
+        SPSPubLogDRbClient.new(host: loghost, port: logport)
+      end
+
       puts 'before reg'.info if @debug
       reg = reghost ? RemoteDwsRegistry.new(domain: reghost) : nil
 
-      @rs = rs = RSFServices.new reg, package_basepath: package_basepath, 
-          log: log, app_rsf: app_rsf, debug: debug
-      
+      @rs = rs = RSFServices.new reg, package_basepath: package_basepath,
+          log: log2, app_rsf: app_rsf, debug: debug
+
       @rs.services['sps'] = SPSPub.new(host: spshost) if spshost
-      
+
       puts 'ready'
 
     end
@@ -45,34 +50,34 @@ module Rse
 
     end
   end
-  
+
   class Subscriber < SPSSub
-    
+
     def initialize(host: 'rse.home', spshost: 'sps.home')
-      
+
       @rsc = RSC.new(host)
       super(host: spshost)
 
     end
-    
+
     def subscribe(topic: 'rse/#')
-      
+
       super(topic: topic) do |msg, topic|
-      
+
         a = topic.split('/')[1..-1]
-        
+
         if a.length < 2 then
-          
+
           begin
             r = @rsc.run_job(a.first, msg)
           rescue
             self.notice 'rse_result: no job ' + a.first
           end
-          
+
           self.notice 'rse_result: ' + r.inspect
-          
+
         else
-          
+
           package, job = a
 
           begin
@@ -80,122 +85,150 @@ module Rse
           rescue
             self.notice 'rse_result: no job ' + a.first
           end
-          
+
           self.notice "rse_result/%s/%s: %s" % [package, job, r.inspect ]
         end
-        
+
       end
-    end  
-    
-  end  
+    end
+
+  end
 
 end
 
 
 module RseProxy
-    
+
   class PassThru
 
-    def initialize(serversx=[], servers: serversx, log: nil )
-      
+    # job_filter (which is optional) expects a block
+    #
+    def initialize(serversx=[], servers: serversx, log: nil, jobfilter: nil )
+
       DRb.start_service
-      
+
       @servers = servers.map do |x|
-        
-        host, port = x.is_a?(Hash) ? [x[:host], x[:port]] : x        
-        
+
+        host, port = x.is_a?(Hash) ? [x[:host], x[:port]] : x
+
       end
-      
+
       @log = Logger.new log, 'daily' if log
-      
+
+      @rse = nil
+      @jobfilter = jobfilter
+
     end
 
     def delete()
       fetch_server() { |rse| rse.delete }
     end
-                     
+
     def get()
       fetch_server() { |rse| rse.get }
     end
-                     
+
     def put()
       fetch_server() { |rse| rse.put }
     end
-                     
+
     def post()
       fetch_server() { |rse| rse.post }
     end
-      
+
     def package_methods(name)
       fetch_server() { |rse| rse.package_methods(name) }
     end
-      
+
     def run_job(package, job, params={}, *args)
-      
-      @log.info "package: %s, job: %s, params: %s, args: %s" \
-          % [package, job, params, args] if @log
-      
-      fetch_server() { |rse| rse.run_job package, job, params, *args    }
-      
-    end
-    
-    def fetch_server()
-      
-      begin
-        
-        servers = @servers.clone
-          
-        server = servers.shift
-        host, port = server
-        rse = DRbObject.new_with_uri("druby://#{host}:#{port}")    
-        yield(rse) if block_given?
-        
-      rescue
-        
-        puts ($!).inspect
-        @log.warn "server #{host}:#{port} down" if @log
-        retry if servers.any?        
-        
+
+      if @log then
+        @log.info "package: %s, job: %s, " +
+            "params: %s, args: %s" % [package, job, params, args]
       end
-      
+
+      #we pass in the job to determine the server
+      fetch_server(job) do |rse|
+        rse.run_job package, job, params, *args
+      end
+
+    end
+
+    def fetch_server(job=nil)
+
+      host, port = nil, nil
+
+      exit if @servers.empty?
+
+      # using a job filter we can send a job (by checking its name) to
+      # a specific server
+      #
+      r = @jobfilter.call(job) if job and @jobfilter.is_a? Proc
+
+      server = if r then
+        r
+      else
+        @log.info 'job: ' + job.inspect if @log
+        @servers.first
+      end
+
+      puts 'server: ' + server.inspect
+
+      begin
+        host, port = server
+        @rse = DRbObject.new_with_uri("druby://#{host}:#{port}")
+
+        yield(@rse) if block_given?
+
+      rescue
+
+        @servers.shift unless port.to_i > 61005
+        @rse = nil
+        puts ($!).inspect
+        @log.warn "server #{host}:#{port} #{($!).inspect}" if @log
+        #sleep 1
+        #jr20210725 retry
+
+
+      end
+
     end
 
     def type()
       fetch_server() { |rse| rse.type }
     end
-    
+
     def type=(s)
       fetch_server() { |rse| rse.type=(s) }
     end
-    
+
     private
-  
-    def method_missing(method_name, *args)    
+
+    def method_missing(method_name, *args)
       fetch_server() { |rse| rse.get.method(method_name).call(*args) }
-    end    
-      
-    
+    end
+
+
   end
 
   class Server
-    
-    def initialize(host: 'rse.home', port: '61000', servers: [], log: nil, 
-                   spshost: 'sps.home')
-      
+
+    def initialize(host: 'rse.home', port: '61000', servers: [], log: nil,
+                   spshost: 'sps.home', jobfilter: nil)
+
       @host, @port, @servers, @log = host, port, servers, log
       @spshost = spshost
-      
+      @jobfilter = jobfilter
+
     end
-    
+
     def start()
-      
-      DRb.start_service "druby://#{@host}:#{@port}", 
-          PassThru.new(@servers, log: @log)
-      DRb.thread.join      
-      
+
+      DRb.start_service "druby://#{@host}:#{@port}",
+          PassThru.new(@servers, log: @log, jobfilter: @jobfilter)
+      DRb.thread.join
+
     end
   end
-  
+
 end
-
-
--- rse.gemspec
+++ rse.gemspec
@@ -1,18 +1,18 @@
 Gem::Specification.new do |s|
   s.name = 'rse'
-  s.version = '0.3.0'
+  s.version = '0.4.0'
   s.summary = 'Executes Ruby jobs (using the rsf_services gem) from ' + 
       'a DRb server.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/rse.rb']
   s.add_runtime_dependency('sps-pub', '~> 0.5', '>=0.5.5')
   s.add_runtime_dependency('sps-sub', '~> 0.3', '>=0.3.7')
-  s.add_runtime_dependency('rsf_services', '~> 0.8', '>=0.8.0')
+  s.add_runtime_dependency('rsf_services', '~> 0.9', '>=0.9.5')
   s.add_runtime_dependency('remote_dwsregistry', '~> 0.4', '>=0.4.1')
-  s.add_runtime_dependency('spspublog_drb_client', '~> 0.1', '>=0.1.1')
+  s.add_runtime_dependency('spspublog_drb_client', '~> 0.2', '>=0.2.1')
   s.signing_key = '../privatekeys/rse.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/rse'
 end
